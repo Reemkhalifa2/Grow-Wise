3bd3da824303ae0f129113c79d3523655f35af64
ReemALadawi <aladawi12reem6@gmail.com>
2026-07-24 17:15:16 +0400

    Investment-Goal-Management-Platform

---
+++ src/main/java/com/example/InvestmentGoalManagementPlatform/InvestmentGoalManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.InvestmentGoalManagementPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentGoalManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentGoalManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InvestmentGoalManagementPlatform/InvestmentGoalManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InvestmentGoalManagementPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentGoalManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
