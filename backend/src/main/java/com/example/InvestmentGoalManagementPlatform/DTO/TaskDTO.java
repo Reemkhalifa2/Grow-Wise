3528c0acf6763a948b0b359d8e467b278810820e
ReemALadawi <aladawi12reem6@gmail.com>
2026-07-26 12:38:33 +0400

    add stock, streak, task dtos

---
+++ backend/src/main/java/com/example/InvestmentGoalManagementPlatform/DTO/TaskDTO.java
@@ -0,0 +1,49 @@
+package com.example.InvestmentGoalManagementPlatform.DTO;
+
+import com.example.InvestmentGoalManagementPlatform.Entities.Task;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskDTO {
+
+    @NotBlank(message = "Task title is required")
+    private String title;
+
+    private String description;
+
+    @FutureOrPresent(message = "Due date cannot be in the past")
+    private LocalDate dueDate;
+
+    private Boolean completed;
+
+    private Integer userId;
+
+
+    public Task toEntity() {
+        Task task = new Task();
+
+        task.setTitle(title);
+        task.setDescription(description);
+        task.setDueDate(dueDate);
+        task.setCompleted(completed);
+
+        return task;
+    }
+
+
+    public void applyTo(Task task) {
+        task.setTitle(title);
+        task.setDescription(description);
+        task.setDueDate(dueDate);
+        task.setCompleted(completed);
+    }
+}
