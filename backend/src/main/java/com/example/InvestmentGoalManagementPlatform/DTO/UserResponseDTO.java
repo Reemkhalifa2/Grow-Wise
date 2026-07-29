e7b122b930d09c33599725b2474b5fa0d39e134a
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-07-29 12:08:24 +0400

    add method from entity to return dto to entity

--- backend/src/main/java/com/example/InvestmentGoalManagementPlatform/DTO/UserResponseDTO.java
+++ backend/src/main/java/com/example/InvestmentGoalManagementPlatform/DTO/UserResponseDTO.java
@@ -1,11 +1,14 @@
 package com.example.InvestmentGoalManagementPlatform.DTO;
 
 
+import com.example.InvestmentGoalManagementPlatform.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -16,4 +19,27 @@ public class UserResponseDTO {
     private String email;
     private Integer monthlySalary;
     private Integer monthlyExpenses;
+
+    public static UserResponseDTO fromEntity(User user) {
+
+        UserResponseDTO dto = new UserResponseDTO();
+
+        dto.setId(user.getId());
+        dto.setFullName(user.getFullName());
+        dto.setEmail(user.getEmail());
+        dto.setMonthlySalary(user.getMonthlySalary());
+        dto.setMonthlyExpenses(user.getMonthlyExpenses());
+
+        return dto;
+    }
+    public static List<UserResponseDTO> fromEntity(List<User> users) {
+
+        List<UserResponseDTO> userDTOList = new ArrayList<>();
+
+        for (User user : users) {
+            userDTOList.add(fromEntity(user));
+        }
+
+        return userDTOList;
+    }
 }
